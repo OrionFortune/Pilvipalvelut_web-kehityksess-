0ad5bb09927e4c0ad54c8ddc5fbb93f87decba7d
OrionFortune <goodgamepro75@gmail.com>
2026-04-22 17:04:17 +0300

    Fix base path and script source

--- matrix-way/vite.config.ts
+++ matrix-way/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Pilvipalvelut_web-kehityksess-/matrix-way/', 
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  }
 })
