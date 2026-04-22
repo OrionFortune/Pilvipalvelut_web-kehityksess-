d23c02495b00ca08644ecfc92602fbbd4302386d
OrionFortune <goodgamepro75@gmail.com>
2026-04-22 17:34:45 +0300

    Deploy correct paths

--- matrix-way/vite.config.ts
+++ matrix-way/vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Pilvipalvelut_web-kehityksess-/matrix-way/', 
+  base: './', 
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist', 
   }
 })
